7cf95060fc53066c9e76bab3710fc4d580212458
Val <poklad_val@rambler.ru>
2023-03-22 15:44:59 +0500

    Poklad Forever

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require "dbconnect.php";
-$result = $conn->query("SELECT *  FROM Novie") ;
+$result = $conn->query("SELECT *  FROM Movie") ;
 echo "<h2>Фильмы</h2>";
 while ($row = $result->fetch()) {
     echo $row['name']."<br>";
