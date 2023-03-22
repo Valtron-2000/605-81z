d358905383ab2cdba37e9665bd55aa06f6adf965
Val <poklad_val@rambler.ru>
2023-03-22 15:43:47 +0500

    Poklad Forever

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
+require 'C:\OSPanel\vendor\autoload.php'; //загрузка всех установленных библиотек
 use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
 if (file_exists(__DIR__."/.env"))
 {
